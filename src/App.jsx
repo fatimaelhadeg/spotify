7d43b850a63b53989f6692ea976a79d183c75d5f
fatimaelhadeg <fatemaelhadeg1@gmail.com>
2025-11-13 13:51:35 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen bg-black">
+        <div className="h-[90%] flex">
+          <Sidebar />
+          <div>
+            <Player />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
